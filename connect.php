66995e518b9bbdff5a19c2b958a2c07994460c40
Thanapat Sirikham <planveler@gmail.com>
2020-11-19 22:40:33 +0700

    check

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
 
-    $conn = mysqli_connect("localhost", "root", "", "loginadminuser");
+    $conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "bf96ee9df22d09", "1073d9c7", "loginadminuser");
 
     if (!$conn) {
         die("Failed to connect to database " . mysqli_error($conn));
