bba48771754be1d50f492ac96a6b8aed488ab484
Thanapat Sirikham <planveler@gmail.com>
2020-11-20 16:47:05 +0700

    check

--- connect.php
+++ connect.php
@@ -5,3 +5,6 @@
     
     $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (!$db) {
+        die("Failed to connec to databse " . mysqli_error($db));
+    }
