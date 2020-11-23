d8707ca8a45e9eb8ced77440037ab46c0cfcb1f1
Thanapat Sirikham <planveler@gmail.com>
2020-11-24 02:01:24 +0700

    check

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -1,87 +1,43 @@
 <?php
-include("connection.php");
-if(isset($_POST["email"]) && (!empty($_POST["email"]))){
-$email = $_POST["email"];
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$email = filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$email) {
-   $error .="<p>Invalid email address please type a valid email address!</p>";
-   }else{
-   $sel_query = "SELECT * FROM `user` WHERE email='".$email."'";
-   $results = mysqli_query($con,$sel_query);
-   $row = mysqli_num_rows($results);
-   if ($row==""){
-   $error .= "<p>No user is registered with this email address!</p>";
-   }
-  }
-   if($error!=""){
-   echo "<div class='error'>".$error."</div>
-   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
-   }else{
-   $expFormat = mktime(
-   date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
-   );
-   $expDate = date("Y-m-d H:i:s",$expFormat);
-   $key = md5(2418*2+$email);
-   $addKey = substr(md5(uniqid(rand(),1)),3,10);
-   $key = $key . $addKey;
-// Insert Temp Table
-mysqli_query($con,
-"INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
-VALUES ('".$email."', '".$key."', '".$expDate."');");
- 
-$output='<p>Dear user,</p>';
-$output.='<p>Please click on the following link to reset your password.</p>';
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p><a href="https://www.allphptricks.com/forgot-password/reset-password.php?
-key='.$key.'&email='.$email.'&action=reset" target="_blank">
-https://www.allphptricks.com/forgot-password/reset-password.php
-?key='.$key.'&email='.$email.'&action=reset</a></p>'; 
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p>Please be sure to copy the entire link into your browser.
-The link will expire after 1 day for security reason.</p>';
-$output.='<p>If you did not request this forgotten password email, no action 
-is needed, your password will not be reset. However, you may want to log into 
-your account and change your security password as someone may have guessed it.</p>';   
-$output.='<p>Thanks,</p>';
-$output.='<p>AllPHPTricks Team</p>';
-$body = $output; 
-$subject = "Password Recovery - AllPHPTricks.com";
- 
-$email_to = $email;
-$fromserver = "noreply@yourwebsite.com"; 
-require("PHPMailer/PHPMailerAutoload.php");
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Host = "us-cdbr-east-02.cleardb.com"; // Enter your host here
-$mail->SMTPAuth = true;
-$mail->Username = "planveler@gmail.com"; // Enter your email here
-$mail->Password = "planveler123"; //Enter your password here
-$mail->Port = 25;
-$mail->IsHTML(true);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require_once "vendor/autoload.php";
+require_once "PHPMailer/class.phpmailer.php";
+
+$mail = new PHPMailer(true);
+
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "planveler@gmail.com";                 
+$mail->Password = "planveler1234";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to
+$mail->Port = 587;                                   
+
 $mail->From = "planveler@gmail.com";
 $mail->FromName = "Planveler Team";
-$mail->Sender = $fromserver; // indicates ReturnPath header
-$mail->Subject = $subject;
-$mail->Body = $body;
-$mail->AddAddress($email_to);
-if(!$mail->Send()){
-echo "Mailer Error: " . $mail->ErrorInfo;
-}else{
-echo "<div class='error'>
-<p>An email has been sent to you with instructions on how to reset your password.</p>
-</div><br /><br /><br />";
- }
-   }
-}else{
-?>
-<form method="post" action="" name="reset"><br /><br />
-<label><strong>Enter Your Email Address:</strong></label><br /><br />
-<input type="email" name="email" placeholder="username@email.com" />
-<br /><br />
-<input type="submit" value="Reset Password"/>
-</form>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<?php } ?>
+
+$mail->addAddress("0904283767.aa@gmail.com", "BetrayedW");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
