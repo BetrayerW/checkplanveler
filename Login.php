403d4cb4e01d167de16424a92fdca15bc917e64f
Thanapat Sirikham <planveler@gmail.com>
2020-11-23 11:30:48 +0700

    check

--- Login.php
+++ Login.php
@@ -25,24 +25,24 @@
 </head>
 
 <body onclick="closeNav()">
-<script>
-  function onSignIn(userInfo) {
-    var result = '';
-    // Result: <textarea id="result"></textarea> อยู่บนscript ล่าง class = g-signin2
-    // Useful data for your client-side scripts:
-    var profile = userInfo.getBasicProfile();
-    
-    result+= "ID: "+profile.getId()+"\n";
-    result+= "Full Name:  "+profile.getName()+"\n";
-    result+= "Given Name: "+profile.getGivenName()+"\n";
-    result+= "Family Name: "+profile.getFamilyName()+"\n";
-    result+= "Email: "+profile.getEmail()+"\n";
-    result+= "ID Token: "+userInfo.getAuthResponse().id_token+"\n";
-    
-    document.getElementById("result").value = result;
-  };
-</script>
-    
+    <script>
+        function onSignIn(userInfo) {
+            var result = '';
+            // Result: <textarea id="result"></textarea> อยู่บนscript ล่าง class = g-signin2
+            // Useful data for your client-side scripts:
+            var profile = userInfo.getBasicProfile();
+
+            result += "ID: " + profile.getId() + "\n";
+            result += "Full Name:  " + profile.getName() + "\n";
+            result += "Given Name: " + profile.getGivenName() + "\n";
+            result += "Family Name: " + profile.getFamilyName() + "\n";
+            result += "Email: " + profile.getEmail() + "\n";
+            result += "ID Token: " + userInfo.getAuthResponse().id_token + "\n";
+
+            document.getElementById("result").value = result;
+        };
+    </script>
+
     <div id="mySidenav" class="sidenav" onclick="event.stopPropagation();">
         <span style="font-size:35px;cursor:pointer;position:absolute;top: 0;right: 5;" onclick="closeNav()">&times;</span>
         <div style="display: block; color: #948BFF; text-align: center;">ชื่อเราเองจ้า</div>
@@ -60,7 +60,8 @@
         <div style="position: absolute;bottom: 0px;">
             <p>
                 <p>
-                    <p></p><a href="Login.php">Login</a></p>
+                    <p></p><a href="Login.php">Login</a>
+                </p>
         </div>
     </div>
 
@@ -69,10 +70,10 @@
             <div id="navbar">
                 <span style="font-size:35px;cursor:pointer;" onclick="openNav(event)">&#9776;</span>
 
-                <span class="icon"> <a href="mainpage.php"> 
-    <img src="/pic/66.png" width="160" height="90" alt="" loading="lazy">
-  </a>
- </span>
+                <span class="icon"> <a href="mainpage.php">
+                        <img src="/pic/66.png" width="160" height="90" alt="" loading="lazy">
+                    </a>
+                </span>
                 <div class="col-lg-2 col-md-2 col-lg-2 offset-lg-6 offset-9" style="padding-right:20px;">
                     <div class="row" style="float: right;">
                         <div class="loginbar">
@@ -106,7 +107,7 @@
             <div class="col-lg-5">
                 <div class="login-1">
                     <div class="content-4">login with</div><br>
-                    <div class="login-btn"><button class="btn btn-lg btn-facebook btn-block text-uppercase" style="width: 280px;" type="submit"><i class="fa fa-facebook" style="margin-right: 20px;"></i>  Sign in with Facebook</button>
+                    <div class="login-btn"><button class="btn btn-lg btn-facebook btn-block text-uppercase" style="width: 280px;" type="submit"><i class="fa fa-facebook" style="margin-right: 20px;"></i> Sign in with Facebook</button>
                         <button class="btn btn-lg btn-google btn-block text-uppercase" style="width: 280px;" type="submit"><i class="fa fa-google" style="color: red; font-size: 20px;margin-right: 30px;"></i> Sign in with Google</button>
                     </div>
                 </div>
@@ -116,56 +117,58 @@
             </div>
             <script>
                 function onSignIn(userInfo) {
-                var result = '';
-                // Result: <textarea id="result"></textarea> อยู่บนscript ล่าง class = g-signin2
-                Useful data for your client-side scripts:
-                var profile = userInfo.getBasicProfile();
-    
-                 result+= "ID: "+profile.getId()+"\n";
-                result+= "Full Name:  "+profile.getName()+"\n";
-                result+= "Given Name: "+profile.getGivenName()+"\n";
-                result+= "Family Name: "+profile.getFamilyName()+"\n";
-                result+= "Email: "+profile.getEmail()+"\n";
-                result+= "ID Token: "+userInfo.getAuthResponse().id_token+"\n";
-    
-                document.getElementById("result").value = result;
+                    var result = '';
+                    // Result: <textarea id="result"></textarea> อยู่บนscript ล่าง class = g-signin2
+                    Useful data
+                    for your client - side scripts:
+                        var profile = userInfo.getBasicProfile();
+
+                    result += "ID: " + profile.getId() + "\n";
+                    result += "Full Name:  " + profile.getName() + "\n";
+                    result += "Given Name: " + profile.getGivenName() + "\n";
+                    result += "Family Name: " + profile.getFamilyName() + "\n";
+                    result += "Email: " + profile.getEmail() + "\n";
+                    result += "ID Token: " + userInfo.getAuthResponse().id_token + "\n";
+
+                    document.getElementById("result").value = result;
                 };
-                </script>
+            </script>
             <?php if (isset($_SESSION['success'])) : ?>
-        <div class="success">
-            <?php 
-                echo $_SESSION['success'];
-            ?>
-        </div>
-    <?php endif; ?>
+                <div class="success">
+                    <?php
+                    echo $_SESSION['success'];
+                    ?>
+                </div>
+            <?php endif; ?>
 
 
-    <?php if (isset($_SESSION['error'])) : ?>
-        <div class="error">
-            <?php 
-                echo $_SESSION['error'];
-            ?>
-        </div>
-    <?php endif; ?>
+            <?php if (isset($_SESSION['error'])) : ?>
+                <div class="error">
+                    <?php
+                    echo $_SESSION['error'];
+                    ?>
+                </div>
+            <?php endif; ?>
 
             <div class="col-lg-6">
                 <div class="login-2">
                     <div class="content-4 ">login your account</div>
-                    <div class="loginbox"> 
-                    <form action="loginpages.php" method="post">
-                        <input type="username" class="form-control" id="username" placeholder="Email"><br>
-                        <input type="password" class="form-control" id="password" placeholder="Password">
-                        <div>
-                            <a href="frontuser1.php">
-                                <button class="loginbtu" type="submit" name="submit" value="login">Login</button>
-                            </a>
-                            <a href="#forgot">
-                                <div class="optionpassword">forgot password ?</div>
-                            </a>
-                            <a href="register.php">
-                                <div class="optionpassword">create your account</div>
-                            </a>
-                        </div></form>
+                    <div class="loginbox">
+                        <form action="https://checkplanveler.herokuapp.com\loginpages.php" method="post">
+                            <input type="username" class="form-control" id="username" placeholder="Email"><br>
+                            <input type="password" class="form-control" id="password" placeholder="Password">
+                            <div>
+                                <a href="frontuser1.php">
+                                    <button class="loginbtu" type="submit" name="submit" value="login">Login</button>
+                                </a>
+                                <a href="#forgot">
+                                    <div class="optionpassword">forgot password ?</div>
+                                </a>
+                                <a href="register.php">
+                                    <div class="optionpassword">create your account</div>
+                                </a>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -188,8 +191,7 @@
                     <div class="botbar-data">
                         <h1>Contact us</h1>
                         <p>
-                            <p><a href="https://www.google.com/maps/dir/13.7358628,100.7661637/%E0%B8%9E%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%AD%E0%B8%A1%E0%B9%80%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%B2%E0%B8%A5%E0%B8%B2%E0%B8%94%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%9A%E0%B8%B1%E0%B8%87/@13.7354147,100.7609192,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x311d664988a1bedf:0xcc678f180e221cd0!2m2!1d100.7782323!2d13.7298889"
-                                    target="_blank">Address</a>
+                            <p><a href="https://www.google.com/maps/dir/13.7358628,100.7661637/%E0%B8%9E%E0%B8%A3%E0%B8%B0%E0%B8%88%E0%B8%AD%E0%B8%A1%E0%B9%80%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%B2%E0%B8%A5%E0%B8%B2%E0%B8%94%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%9A%E0%B8%B1%E0%B8%87/@13.7354147,100.7609192,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x311d664988a1bedf:0xcc678f180e221cd0!2m2!1d100.7782323!2d13.7298889" target="_blank">Address</a>
                                 <p><a href="">E-mail</a>
                     </div>
                 </div>
@@ -221,10 +223,10 @@
 </body>
 
 </html>
-<?php 
+<?php
 
-    if (isset($_SESSION['success']) || isset($_SESSION['error'])) {
-        session_destroy();
-    }
+if (isset($_SESSION['success']) || isset($_SESSION['error'])) {
+    session_destroy();
+}
 
 ?>
