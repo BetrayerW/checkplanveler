0f05e0405bec91093f4cb3c5fe72dad41e967c2d
Thanapat Sirikham <planveler@gmail.com>
2020-11-23 11:36:45 +0700

    check

--- loginpages.php
+++ loginpages.php
@@ -1,50 +1,46 @@
-<?php 
+<?php
 
-    session_start();
+session_start();
 
-    if (isset($_POST['username'])) {
+if (isset($_POST['username'])) {
 
-        include('connection.php');
+    include('connection.php');
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $passwordenc = md5($password);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passwordenc = md5($password);
 
-        $query = "SELECT * FROM user WHERE username = '$username' AND password = '$passwordenc'";
+    $query = "SELECT * FROM user WHERE username = '$username' AND password = '$passwordenc'";
 
-        $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);
 
-        if (mysqli_num_rows($result) == 1) {
+    if (mysqli_num_rows($result) == 1) {
 
-            $row = mysqli_fetch_array($result);
+        $row = mysqli_fetch_array($result);
 
-            $_SESSION['userid'] = $row['id'];
-            $_SESSION['user'] = $row['firstname'] . " " . $row['lastname'];
-            $_SESSION['userfirstname'] = $row['firstname'];
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['userlastname'] = $row['lastname'];
-            $_SESSION['userlevel'] = $row['userlevel'];
-            $_SESSION['useremail'] = $row['email'];
-            $_SESSION['usertel'] = $row['tel'];
-            $_SESSION['usersex'] = $row['sex'];
-            $_SESSION['userjob'] = $row['job'];
-            $_SESSION['userimage_user'] = $row['image_user'];
+        $_SESSION['userid'] = $row['id'];
+        $_SESSION['user'] = $row['firstname'] . " " . $row['lastname'];
+        $_SESSION['userfirstname'] = $row['firstname'];
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['userlastname'] = $row['lastname'];
+        $_SESSION['userlevel'] = $row['userlevel'];
+        $_SESSION['useremail'] = $row['email'];
+        $_SESSION['usertel'] = $row['tel'];
+        $_SESSION['usersex'] = $row['sex'];
+        $_SESSION['userjob'] = $row['job'];
+        $_SESSION['userimage_user'] = $row['image_user'];
 
-            if ($_SESSION['userlevel'] == 'a') {
-                header("Location: admin_page.php");
-            }
+        if ($_SESSION['userlevel'] == 'a') {
+            header("Location: admin_page.php");
+        }
 
-            if ($_SESSION['userlevel'] == 'm') {
-                echo "hello";
-            }
-        } else {
-            echo "<script>alert('User หรือ Password ไม่ถูกต้อง');</script>";
-            echo "<script>window.history.back()</script>";
-                }
-
-    } 
-    else {
-        header("Location: index.php");
-            }
-            
-?>
+        if ($_SESSION['userlevel'] == 'm') {
+            echo "hello";
+        }
+    } else {
+        echo "<script>alert('User หรือ Password ไม่ถูกต้อง');</script>";
+        echo "<script>window.history.back()</script>";
+    }
+} else {
+    header("Location: index.php");
+}
