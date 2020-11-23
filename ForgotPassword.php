b964d4c9433ad0dc1fd9d711ecab404882b2c310
Thanapat Sirikham <planveler@gmail.com>
2020-11-23 23:14:05 +0700

    check

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -3,17 +3,17 @@ session_start();
 include_once("connection.php");
 if(isset($_POST['submit']))
 {
-    $user_id = $_POST['user_id'];
-    $result = mysqli_query($conn,"SELECT * FROM user_details where user_id='" . $_POST['user_id'] . "'");
+    $user_id = $_POST['username'];
+    $result = mysqli_query($conn,"SELECT * FROM user where username='" . $_POST['username'] . "'");
     $row = mysqli_fetch_assoc($result);
-	$fetch_user_id=$row['user_id'];
-	$email_id=$row['email_id'];
+	$fetch_user_id=$row['username'];
+	$email_id=$row['email'];
 	$password=$row['password'];
 	if($user_id==$fetch_user_id) {
 				$to = $email_id;
                 $subject = "Password";
                 $txt = "Your password is : $password.";
-                $headers = "From: password@studentstutorial.com" . "\r\n" .
+                $headers = "From: planveler@gmail.com" . "\r\n" .
                 "CC: somebodyelse@example.com";
                 mail($to,$subject,$txt,$headers);
 			}
@@ -42,7 +42,7 @@ if(isset($_POST['submit']))
 <h1>Forgot Password<h1>
 <form action='' method='post'>
 <table cellspacing='5' align='center'>
-<tr><td>user id:</td><td><input type='text' name='user_id'/></td></tr>
+<tr><td>username:</td><td><input type='text' name='usernaeme'/></td></tr>
 <tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
 </table>
 </form>
