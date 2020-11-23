2fc5e97786fd1186279841565cdbcc29d8bb3d64
Thanapat Sirikham <planveler@gmail.com>
2020-11-23 23:09:11 +0700

    check

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -1,6 +1,50 @@
+<?php
+session_start();
+include_once("connection.php");
+if(isset($_POST['submit']))
+{
+    $user_id = $_POST['user_id'];
+    $result = mysqli_query($conn,"SELECT * FROM user_details where user_id='" . $_POST['user_id'] . "'");
+    $row = mysqli_fetch_assoc($result);
+	$fetch_user_id=$row['user_id'];
+	$email_id=$row['email_id'];
+	$password=$row['password'];
+	if($user_id==$fetch_user_id) {
+				$to = $email_id;
+                $subject = "Password";
+                $txt = "Your password is : $password.";
+                $headers = "From: password@studentstutorial.com" . "\r\n" .
+                "CC: somebodyelse@example.com";
+                mail($to,$subject,$txt,$headers);
+			}
+				else{
+					echo 'invalid userid';
+				}
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<style type="text/css">
+ input{
+ border:1px solid olive;
+ border-radius:5px;
+ }
+ h1{
+  color:darkgreen;
+  font-size:22px;
+  text-align:center;
+ }
 
-<form id="form1" name="form1"  method="post" action="SendPassword.php" >
-    <label for="email" >Email</label>
-    <input type="email" name="email" autocomplete="off"   placeholder="Enter email" value=""><br />
-    <button type="submit" value="ขอรับรหัสผ่าน" name="submit" >ขอรับรหัสผ่าน</button>
+</style>
+</head>
+<body>
+<h1>Forgot Password<h1>
+<form action='' method='post'>
+<table cellspacing='5' align='center'>
+<tr><td>user id:</td><td><input type='text' name='user_id'/></td></tr>
+<tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
+</table>
 </form>
+</body>
+</html>
--- SendPassword.php
+++ SendPassword.php
@@ -1,57 +1,41 @@
-<html>
+<?php
+if(isset($_GET['code'])) {
 
-<head>
-    <style type="text/css">
-        input {
-            border: 1px solid olive;
-            border-radius: 5px;
-        }
+    $acode = $_GET['code'];}
+    echo $acode;
 
-        h1 {
-            color: darkgreen;
-            font-size: 22px;
-            text-align: center;
-        }
-    </style>
-</head>
+if(isset($_POST['pass'])){
+    $pass = $_POST['pass'];
 
-<body>
-    <h1>Forgot Password<h1>
-            <form action='#' method='post'>
-                <table cellspacing='5' align='center'>
-                    <tr>
-                        <td>Email id:</td>
-                        <td><input type='text' name='email' /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><input type='submit' name='submit' value='Submit' /></td>
-                    </tr>
-                </table>
-            </form>
-            <?php
-            if (isset($_POST['submit'])) {
-                include("connection.php");
-                $mail = $_POST['email'];
-                $q = mysql_query("select * from user where useremail='" . $email . "' "); 
-                $p = mysql_affected_rows();
-                if ($p != 0) {
-                    $res = mysql_fetch_array($q);
-                    $to = $res['useremail'];
-                    $subject = 'Remind password';
-                    $message = 'Your password : ' . $res['userpassword'];
-                    $headers = 'From:planveler@gmail.com';
-                    $m = mail($to, $subject, $message, $headers);
-                    if ($m) {
-                        echo 'Check your inbox in mail';
-                    } else {
-                        echo 'mail is not send';
-                    }
-                } else {
-                    echo 'You entered mail id is not present';
-                }
-            }
-            ?>
-</body>
 
-</html>
+$con=mysqli_connect("localhost", "root", "", "loginadminuser");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+$query = mysqli_query($con,"select * from login where activation_code='$acode'")
+or die(mysqli_error($con)); 
+
+ if (mysqli_num_rows ($query)==1) 
+{
+
+
+
+$query3 = mysqli_query($con,"update login set Password='$pass' where activation_code='$acode'")
+or die(mysqli_error($con)); 
+
+echo 'Password Changed';
+}
+else
+{
+echo 'Wrong CODE';
+
+}}
+
+?>
+
+    <form action="resetpass.php" method="POST">
+    <p>New Password:</p><input type="password" name="pass" />
+    <input type="submit"  name="submit" value="Signup!" />
+    </form>
