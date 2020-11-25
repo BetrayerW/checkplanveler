939228df0691ef8ac624e8060f53bcbf0724bc75
Thanapat Sirikham <planveler@gmail.com>
2020-11-25 18:19:22 +0700

    check

--- example01.php
+++ example01.php
@@ -21,20 +21,20 @@
 <body onclick="closeNav()">
     <div id="mySidenav" class="sidenav" onclick="event.stopPropagation();">
         <span style="font-size:35px;cursor:pointer;position:absolute;top: 0;right: 5;" onclick="closeNav()">&times;</span>
-        <div style="display: block; color: #948BFF; text-align: center;">ชื่อเราเองจ้า</div>
+        <div style="display: block; color: #948BFF; text-align: center;">Guest</div>
 
-        <a href="mainpage.html">หน้าหลัก</a>
-        <a href="mainpage.html">รายการที่บันทึก</a>
-        <a href="mainpage.html">รายการทริปที่แนะนำ</a>
-        <a href="Howtouse.html">แนะนำการใช้งาน</a>
-        <a href="planveler.html">เกี่ยวกับเรา</a>
-        <a href="help.html">คำถามที่พบบ่อย</a>
+        <a href="index.php">หน้าหลัก</a>
+        <a href="index.php">รายการที่บันทึก</a>
+        <a href="index.php">รายการทริปที่แนะนำ</a>
+        <a href="Howtouse.php">แนะนำการใช้งาน</a>
+        <a href="planveler.php">เกี่ยวกับเรา</a>
+        <a href="help.php">คำถามที่พบบ่อย</a>
 
 
         <div style="position: absolute;bottom: 0px;">
             <p>
                 <p>
-                    <p></p><a href="Login.html">Login</a></p>
+                    <p></p><a href="Login.php">Login</a></p>
         </div>
     </div>
 
@@ -43,7 +43,7 @@
             <div id="navbar">
                 <span style="font-size:35px;cursor:pointer;" onclick="openNav(event)">&#9776;</span>
 
-                <span class="icon"> <a href="mainpage.html"> 
+                <span class="icon"> <a href="index.php"> 
     <img src="/pic/66.png" width="160" height="90" alt="" loading="lazy">
   </a>
  </span>
@@ -57,8 +57,8 @@
                     <div class="row" style="float: right;margin-right: auto;">
                         <div class="col-auto">
                             <div class="loginbar">
-                                <a href="/register.html" style=><img src="/pic/Group 5.png" alt="Register"></a>
-                                <a href="Login.html" style=><img src="/pic/Group 7.png" alt="Login"></a>
+                                <a href="/register.php" style=><img src="/pic/Group 5.png" alt="Register"></a>
+                                <a href="Login.php" style=><img src="/pic/Group 7.png" alt="Login"></a>
                             </div>
                         </div>
                     </div>
@@ -432,8 +432,8 @@
                     <div class="botbar-data">
                         <h1>About us</h1>
                         <p>
-                            <p><a href="planveler.html">What's Planveler?</a>
-                                <p><a href="Howtouse.html">How to use</a>
+                            <p><a href="planveler.php">What's Planveler?</a>
+                                <p><a href="Howtouse.php">How to use</a>
                     </div>
                 </div>
                 <div class="col-auto">
@@ -450,8 +450,8 @@
                     <div class="botbar-data">
                         <h1>Planveler Policies</h1>
                         <p>
-                            <p><a href="Terms&Condition.html">Terms & Conditions</a>
-                                <p><a href="Help.html">Help Center</a>
+                            <p><a href="Terms&Condition.php">Terms & Conditions</a>
+                                <p><a href="Help.php">Help Center</a>
                     </div>
                 </div>
                 <div class="col-auto">
@@ -460,7 +460,7 @@
                         <p>
                             <a href="https://www.facebook.com/Planveler-109500384231284"><img src="/pic/Facebook logo 2.png"></a>
                             <p></p>
-                            <a href="#line"><img src="/pic/Line logo 1.png"></a>
+                            <a href="https://lin.ee/QXFQsOg"><img src="/pic/Line logo 1.png"></a>
                             <a href="https://www.instagram.com/planveler.official/"><img src="/pic/Instragram Logo 2.png"></a>
                     </div>
                 </div>
