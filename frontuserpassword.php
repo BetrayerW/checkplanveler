595946e9acdfa9633eeaf9bca3694e45e290e15e
Thanapat Sirikham <planveler@gmail.com>
2020-11-26 09:38:04 +0700

    check

--- frontuserpassword.php
+++ frontuserpassword.php
@@ -129,7 +129,7 @@
                 </div>
             </div>
             <div class="col-3 offset-9">
-                <a href="userprofileaddressedit.html"> <a href="userprofilepasswordedit.html">
+                <a href="userprofileaddressedit.php"> <a href="userprofilepasswordedit.php">
                     <input type="button" class="profile-edit-btn" name="btnAddMore" value="Edit" /></a>
                 </a>
             </div>
